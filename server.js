a2d1578221cb011b74fc72612ad227a9816c4181
Bryan Cipriano <ciprianot.bryan@gmail.com>
2019-11-01 02:48:18 -0500

    respuestas coherentes

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const response = require('./network/response');
+
 const router = express.Router();
 
 let app = express();
 app.use(bodyParser.json());
 app.use(router);
+
+router.get('/message', (req, res) => {
+    response.success(req, res, 'Lista de mensajes');
+})
 
 router.get('/cabecera', (req, res) => {
     console.log(req.headers);
@@ -12,6 +18,13 @@ router.get('/cabecera', (req, res) => {
         "custom-header": "Nuestro valor personalizado"
     });
     res.send('Información de la cabecera');
+});
+
+router.post('/user', (req, res) => {
+    res.status(200).send({
+        error: '',
+        body: 'Creado correctamente'
+    });
 });
 
 router.get('/', (req, res) => {
