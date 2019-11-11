48ec90035c72a9f7cf7cfc2bf23a5d223c5ee933
Bryan Cipriano <ciprianot.bryan@gmail.com>
2019-11-10 23:58:18 -0500

    MongoDB I: Almacenar y leer datos

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,23 @@
-const list = [];
+const db = require('mongoose');
+const Model = require('./model');
+
+const pathCredential = '';
+
+db.Promise = global.Promise;
+db.connect(pathCredential, {
+    useNewUrlParser: true
+});
+
+console.log('[db] Conectada con exito');
 
 addMessage = message => {
-    list.push(message);
+    const myMessage = new Model(message);
+    myMessage.save();
 };
 
-getMessages = () => {
-    return list;
+getMessages = async () => {
+    const messages  = await Model.find();
+    return messages;
 };
 
 module.exports = {
--- db.js
+++
@@ -1,3 +0,0 @@
-const db = require('mongoose');
-
-db.Promise = global.Promise;
